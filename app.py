ce38926f58d694f23dacf62fe2fddaf2c08eb62e
Zhengkii-bis <simballajayson@gmail.com>
2025-02-25 15:14:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import textstat
-from gingerit.gingerit import GingerIt
+from gingerit import GingerIt
 from symspellpy import SymSpell, Verbosity
 import re
 import os
