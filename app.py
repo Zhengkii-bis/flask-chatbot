e7a8c5c087c247641927d1a75377d8eb4af3cdee
Zhengkii-bis <simballajayson@gmail.com>
2025-02-25 16:34:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,9 @@ def check_grammar(text):
     else:
         return "Error checking grammar."
        
-   def analyze_organization(text):
+    
+    
+def analyze_organization(text):
     sentences = re.split(r'(?<=[.!?]) +', text)
     num_sentences = len(sentences)
     avg_sentence_length = sum(len(s.split()) for s in sentences) / num_sentences if num_sentences else 0
